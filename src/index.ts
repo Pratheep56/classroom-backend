9abe5ea32fbb1ee88988a2604a009ef69d43f53c
Pratheep56 <pratheep0610@gmail.com>
2026-08-06 01:55:43 +0530

    implement fetch teachers and create classes

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import securityMiddleware from "./middleware/security";
 import {toNodeHandler} from "better-auth/node";
 import {auth} from "./lib/auth";
+import usersRouter from "./routes/users"
+import classesRouter from "./routes/classes"
 
 const app = express()
 const PORT = 8000
@@ -23,6 +25,9 @@ app.use(express.json());
 app.use(securityMiddleware);
 
 app.use("/api/subjects", subjectRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/classes", classesRouter);
+
 app.get("/", (req, res) => {
     res.send("Hello, welcome to the Classroom API!");
 });
